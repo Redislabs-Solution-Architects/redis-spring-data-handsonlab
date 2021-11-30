4df74ca16efdba46c1a5eb4f5fa2b4ada42fff8e
Lars Rosenquist <rosenql@gmail.com>
2021-11-30 11:01:23 +0100

    Simplified TestContainers testing code

--- exercises/exercise5-datageneration/src/test/java/com/redis/lars/exercise5/Exercise5ApplicationTests.java
+++ exercises/exercise5-datageneration/src/test/java/com/redis/lars/exercise5/Exercise5ApplicationTests.java
@@ -1,40 +1,29 @@
 package com.redis.lars.exercise5;
 
-import org.junit.ClassRule;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.util.TestPropertyValues;
-import org.springframework.context.ApplicationContextInitializer;
-import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.utility.DockerImageName;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
-@ContextConfiguration(initializers = { Exercise5ApplicationTests.Initializer.class })
+@SpringJUnitConfig
+@Testcontainers
 class Exercise5ApplicationTests {
 
 	static final String REDIS_IMAGE = "redislabs/redismod:latest";
 	static final int REDIS_PORT = 6379;
 
-	@ClassRule
-	@SuppressWarnings("rawtypes")
-	public static GenericContainer redisContainer = new GenericContainer(DockerImageName.parse(REDIS_IMAGE))
+	@Container
+	static GenericContainer<?> redisContainer = new GenericContainer<>(DockerImageName.parse(REDIS_IMAGE))
 			.withExposedPorts(REDIS_PORT);
 
-	static class Initializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
-		public void initialize(ConfigurableApplicationContext configurableApplicationContext) {
-
-			redisContainer.start();
-
-			TestPropertyValues
-					.of("spring.redis.host=" + redisContainer.getHost(),
-							"spring.redis.port=" + redisContainer.getFirstMappedPort())
-					.applyTo(configurableApplicationContext.getEnvironment());
-		}
+	@DynamicPropertySource
+	static void redisProperties(DynamicPropertyRegistry registry) {
+		registry.add("redis.host", redisContainer::getContainerIpAddress);
+		registry.add("redis.posrt", redisContainer::getFirstMappedPort);
 	}
 
 	@Test
